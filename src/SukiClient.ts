606d9929fc91e3e90052855aad2aefc2e8198877
Niskii3 <psychoniskii2@gmail.com>
2022-04-14 21:11:12 -0300

    feat(Client): add request and debug

--- src/SukiClient.ts
+++ src/SukiClient.ts
@@ -1,4 +1,6 @@
 import { Client, GatewayIntentBits } from 'discord.js';
+import { request, Dispatcher } from 'undici';
+import { UrlObject } from 'url';
 
 import { EventManager, CommandManager, DatabaseManager, LocaleManager } from './Managers';
 
@@ -15,6 +17,10 @@ export default class SukiClient extends Client {
   playerManager: PlayerManager;
   guildDB: typeof dbGuild;
   userDB: typeof dbUser;
+  request: (
+    url: string | URL | UrlObject,
+    options?: { dispatcher?: Dispatcher } & Omit<Dispatcher.RequestOptions, 'origin' | 'path' | 'method'> & Partial<Pick<Dispatcher.RequestOptions, 'method'>>,
+  ) => Promise<Dispatcher.ResponseData>;
 
   constructor() {
     super({
@@ -36,6 +42,7 @@ export default class SukiClient extends Client {
     this.developers = ['847865068657836033', '600804786492932101', '680943469228982357', '343778106340802580', '431768491759239211', '689265428769669155'];
     this.guildDB = dbGuild;
     this.userDB = dbUser;
+    this.request = request;
   }
 
   connectLavaLink(): void {
@@ -50,5 +57,7 @@ export default class SukiClient extends Client {
     new EventManager(this).loadEvents(__dirname + '/Listeners');
     new DatabaseManager(this).execute();
     new LocaleManager(this).execute();
+
+    this.on('debug', (d) => console.log(d));
   }
 }
