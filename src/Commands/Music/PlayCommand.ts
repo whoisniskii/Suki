36b3ce027488a83e6913803bdf0bfde0867b90ce
Niskii3 <psychoniskii2@gmail.com>
2022-04-14 21:12:24 -0300

    feat: add autocomplete in play command

--- src/Commands/Music/PlayCommand.ts
+++ src/Commands/Music/PlayCommand.ts
@@ -1,7 +1,9 @@
-import { ApplicationCommandOptionType, CommandInteraction, GuildMember } from 'discord.js';
+/* eslint-disable no-var */
+import { ApplicationCommandOptionType, AutocompleteInteraction, CommandInteraction, GuildMember } from 'discord.js';
 import CommandConstructor from '../../Structures/Command';
 import SukiClient from '../../SukiClient';
 import GuildPlayer from '../../Music/Structures/GuildPlayerManager';
+import { Choices } from '../../typings/index';
 
 export default class PlayCommand extends CommandConstructor {
   constructor(client: SukiClient) {
@@ -15,7 +17,8 @@ export default class PlayCommand extends CommandConstructor {
           name: 'track',
           description: 'Music you want to play.',
           type: ApplicationCommandOptionType.String,
-          required: true
+          required: true,
+          autocomplete: true,
         }]
       }
     }, client);
@@ -27,5 +30,35 @@ export default class PlayCommand extends CommandConstructor {
     const args = interaction.options.get('track');
 
     await player.createPlayer(args?.value as string, interaction.member as GuildMember, interaction, t);
+  }
+
+  async executeAutoComplete(interaction: AutocompleteInteraction, value: string) {
+    if (!value) {
+      interaction.respond([]);
+      return;
+    }
+
+    const res = await this.client.request(`https://suggestqueries-clients6.youtube.com/complete/search?client=youtube&ds=yt&callback=json&q=${encodeURIComponent(value)}`, {
+      headers: {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36'
+      }
+    }).then(async r => Buffer.from(await r.body.arrayBuffer()).toString('latin1'));
+
+    const choices: Choices[] = [];
+
+    const data = res.split('[');
+
+    for (var i = 3, min = Math.min(8 * 2, data.length); i < min; i += 2) {
+      const choice = data[i].split('"')[1].replace(/\\u([0-9a-fA-F]{4})/g, (_, cc) => String.fromCharCode(parseInt(cc, 16)));
+
+      if (choice) {
+        choices.push({
+          name: choice,
+          value: choice
+        });
+      }
+
+      interaction.respond(choices);
+    }
   }
 }
