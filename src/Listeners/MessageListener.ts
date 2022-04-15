2702ffa1d3bcd3f2ebf06f1deeb8fa2a6b818ee7
Niskii3 <psychoniskii2@gmail.com>
2022-04-14 21:11:57 -0300

    fix: button builder in message create

--- src/Listeners/MessageListener.ts
+++ src/Listeners/MessageListener.ts
@@ -1,4 +1,4 @@
-import { EnumResolvers, Message, ActionRowBuilder, ButtonBuilder } from 'discord.js';
+import { Message, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
 import i18next from 'i18next';
 import SukiClient from '../SukiClient';
 
@@ -18,12 +18,15 @@ export default class MessageCreate {
 
     const t = global.t = i18next.getFixedT('en-US');
 
-    const button = new ButtonBuilder()
-      .setURL(`https://discord.com/api/oauth2/authorize?client_id=${this.client.user?.id}&permissions=1512385670486&scope=bot%20applications.commands`)
-      .setLabel(t('events:messageCreate.button'))
-      .setStyle(EnumResolvers.resolveButtonStyle('LINK'));
+    const button = new ButtonBuilder(
+      {
+        style: ButtonStyle.Link,
+        label: t('events:messageCreate.button'),
+        url: `https://discord.com/api/oauth2/authorize?client_id=${this.client.user?.id}&permissions=1512385670486&scope=bot%20applications.commands`
+      }
+    );
 
-    const row = new ActionRowBuilder<ButtonBuilder>().setComponents(button);
+    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button);
 
     if(message.content.match(GetMention(this.client.user?.id as string))) {
       message.reply({ content: t('events:messageCreate.message', { user: message.author.toString() }), components: [row] });
