2b7ccc80aae605363f1c15520ea2cc71ef5c5462
Niskii3 <nicolas.ribeiro.s@outlook.com>
2022-04-17 18:35:09 -0300

    feat(API): add `getLyrics`and `getTrack` method

--- src/APIS/Musixmatch.ts
+++ src/APIS/Musixmatch.ts
@@ -10,6 +10,15 @@ export default class MusixMatch {
     this.client = client;
   }
 
+  async getTrack(params: string) {
+    const result = await this.client.request(`${BASE_URL}track.get?track_isrc=${params}&apikey=${this.apiKey}`)
+      .then((res => res.body.json()));
+
+    if(!result.message.body.track) return null;
+
+    return result.message.body.track;
+  }
+
   async searchTrack(params: string, artist: string) {
     const result = await this.client.request(`${BASE_URL}track.search?q_track=${params}&q_artist=${artist}&apikey=${this.apiKey}`)
       .then((res => res.body.json()));
@@ -17,5 +26,14 @@ export default class MusixMatch {
     if(!result.message.body.track_list.length) return null;
 
     return result.message.body.track_list[0].track;
+  }
+
+  async getLyrics(params: string) {
+    const result = await this.client.request(`${BASE_URL}track.lyrics.get?track_id=${params}&apikey=${this.apiKey}`)
+      .then((res => res.body.json()));
+
+    if(!result.message.body.lyrics) return null;
+
+    return result.message.body.lyrics;
   }
 }
